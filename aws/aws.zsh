04b5176814a3a31b5bc02ce914f7f4f791106acf
Muhammad Umer Azad <umer.azad@gmail.com>
2013-07-14 01:06:33 -0700

    Minor edits + sublime support

---
+++ aws/aws.zsh
@@ -0,0 +1,5 @@
+echo "Loading aws.zsh"
+
+export AWS_CONFIG_FILE=~/.aws/aws.cfg
+#Assuming awscli is installed
+source /usr/local/bin/aws_zsh_completer.sh
---
+++ ipython/aliases.zsh
@@ -0,0 +1,1 @@
+echo "Loading ipython/aliases"
