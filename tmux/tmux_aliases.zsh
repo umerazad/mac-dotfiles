22b507d496acfb43fbd32bf7f19272e547babe87
Muhammad Umer Azad <umer.azad@gmail.com>
2013-09-17 10:08:50 -0700

    Added hosts files to block ad/tracking-cookies

--- tmux/tmux_aliases.zsh
+++ tmux/tmux_aliases.zsh
@@ -1,8 +1,7 @@
 # Session related aliases
-alias tmux="TERM=screen-256color-bce tmux"
 
 alias tls='tmux list-sessions'
-alias tns='tmux new -s'
+alias tns='TERM=screen-256color-bce tmux -2 new -s'
 alias tat='tmux attach -d -t'
 
 # ruby-devenv through tmuxinator
