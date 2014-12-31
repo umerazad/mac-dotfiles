a2040adbde1716ec6b5578c93e44f26372f6db0b
Muhammad Umer Azad <crtstartup@outlook.com>
2014-12-30 21:16:16 -0800

    minor fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -25,11 +25,5 @@ alias sl=ls # often screw this up
 alias afind='ack-grep -il'
 
 alias reload!='. ~/.zshrc'
-alias fe="cd /Users/umerazad/workspace/fe"
-alias feservice="cd /Users/umerazad/workspace/fe/src/AWSLogsService"
-alias fecore="cd /Users/umerazad/workspace/fe/src/AWSLogsCore"
-alias be="cd /Users/umerazad/workspace/be"
-alias beservice="cd /Users/umerazad/workspace/be/src/AWSLogsStagingService"
-alias becore="cd /Users/umerazad/workspace/be/src/AWSLogsCore"
-alias clidev="cd /Users/umerazad/workspace/awslogs/cli/"
-
+alias cj='cd ~/Code/go/src/github.com/umerazad/codejudge'
+alias graphx='cd ~/Code/go/src/github.com/umerazad/graphx'
